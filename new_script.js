7c9d2d90c1ff77b384ae374fce69c6406b95aad7
Francisco Hernández Santos <70912492+pacoDevelop@users.noreply.github.com>
2024-05-20 13:39:34 +0200

    Create new_script.js

---
+++ new_script.js
@@ -0,0 +1,11 @@
+const a = document.querySelectorAll('div[class="ud-btn ud-btn-large ud-btn-link ud-heading-md ud-accordion-panel-toggler accordion-panel-module--panel-toggler--WUiNu accordion-panel-module--outer-panel-toggler--Pxwxc"]');
+
+a.forEach(b => {
+  b.click();
+});
+
+const c = document.querySelectorAll('label[class="curriculum-item-link--progress-toggle--f0M8W ud-toggle-input-container ud-text-sm"]');
+// Iterar sobre los elementos encontrados y hacer algo con ellos
+c.forEach(b => {
+  b.click();
+});
